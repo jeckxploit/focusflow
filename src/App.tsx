57355a9c904918272dbcde3e8a677fec4689ff2c
[dyad] <putanebapak03@gmail.com>
2026-02-11 22:24:01 +0700

    [dyad] Membatalkan refaktor struktur folder dan mengembalikan kode ke kondisi semula - wrote 3 file(s), deleted 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from 'react-hot-toast'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
-import AppLayout from './components/layout/AppLayout'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
@@ -15,22 +14,8 @@ function App() {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        {/* Protected Routes with Layout */}
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <AppLayout>
-              <Dashboard />
-            </AppLayout>
-          </ProtectedRoute>
-        } />
-        <Route path="/projects" element={
-          <ProtectedRoute>
-            <AppLayout>
-              <Projects />
-            </AppLayout>
-          </ProtectedRoute>
-        } />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/projects" element={<ProtectedRoute><Projects /></ProtectedRoute>} />
       </Routes>
     </>
   )
