985063f013a061130e7cefa39a13fc7524d82e98
[dyad] <putanebapak03@gmail.com>
2026-02-12 20:15:06 +0700

    Update 1 file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5hhtoe44anc"
+    "revision": "0.81n03knf0ac"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
